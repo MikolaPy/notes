f3498b9de13405592eb167342ffb5a1b2de2dd5a
MikolaPy <root@DESKTOP-JCIUU4N.localdomain>
2025-07-19 14:00:19 +0300

    add_search_form_to_notes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,7 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import AutoSubmit from "./auto_submit_controller.js"
+
+application.register('auto-submit', AutoSubmit)
 eagerLoadControllersFrom("controllers", application)
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,8 @@ pin "application"
 
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@stimulus-components/auto-submit", to: "@stimulus-components--auto-submit.js" # @6.0.0
